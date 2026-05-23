9051a8aa1e23f0c96321e5c9605a3d136814651b
Jefferson Henrique da Silva <jeffersonhsilva01@gmail.com>
2026-05-23 06:22:07 -0300

    Primeiro commit

---
+++ src/main/java/poo/ex_51/Ex_51.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package poo.ex_51;
+
+/**
+ *
+ * @author jeffe
+ */
+public class Ex_51 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
